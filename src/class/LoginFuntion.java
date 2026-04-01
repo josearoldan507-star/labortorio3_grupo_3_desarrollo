6732cf121926746891b44cb7f0759588b80ac91c
josearoldan507-estrella <josearoldan507@gmail.com>
2026-03-31 19:51:00 -0500

    cree el proyecto3 con sus carpetas

---
+++ src/class/LoginFuntion.java
@@ -0,0 +1,15 @@
+
+
+public class LoginFuntion {
+
+    public class LoginFunction {
+
+    public static boolean validatelogin(String usrname, String password) {
+        return "admin".equals(usrname) && "password".equals(password);
+
+        
+    }
+
+}
+    
+}
